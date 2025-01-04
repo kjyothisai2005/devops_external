4d0a7dcc88310247df44b474a1801151731809c1
kjyothisai2005 <kavitijyothisai@gmail.com>
2025-01-04 14:34:25 +0530

    second commit

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 const regristrationform=document.getElementById("registrationform");
 const registration=document.getElementById("Registration");
 const profilepage=document.getElementById("profile-page");
-
 const profilename=document.getElementById("profilename");
 const profileemail=document.getElementById("profileemail");
-
 registration.addEventListener("submit",function (e){
 e.preventDefault();
 const name=document.getElementById("name").value;
 const email=document.getElementById("email").value;
-
 profilename.textContent=name;
 profileemail.textContent=email;
-
 regristrationform.style.display='none';
 profilepage.style.display='block';
 
